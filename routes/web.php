ddf531f69937c2d1612466b4628d538900d5ac7d
Joel Paulsson <105986177+paulssonjoel@users.noreply.github.com>
2022-05-20 21:00:54 +0200

    Add route comments

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Unauthenticated login
 Route::get('/login', '\App\Http\Controllers\Web\LoginController@form');
 Route::post('/login', '\App\Http\Controllers\Web\LoginController@login');
 
+// Authenticated admin panel
 Route::middleware(['\App\Http\Middleware\IsAdminMiddleware'])->prefix('admin')->group(function () {
+    // Books
     Route::get('/books/create', '\App\Http\Controllers\Web\BookController@create');
     Route::post('/books/create', '\App\Http\Controllers\Web\BookController@store');
     Route::get('/books/{book}', '\App\Http\Controllers\Web\BookController@edit');
     Route::post('/books/{book}', '\App\Http\Controllers\Web\BookController@update');
     Route::post('/books/{book}/delete', '\App\Http\Controllers\Web\BookController@destroy');
 
+    // Authors
     Route::get('/authors/create', '\App\Http\Controllers\Web\AuthorController@create');
     Route::post('/authors/create', '\App\Http\Controllers\Web\AuthorController@store');
     Route::get('/authors/{author}', '\App\Http\Controllers\Web\AuthorController@edit');
     Route::post('/authors/{author}', '\App\Http\Controllers\Web\AuthorController@update');
     Route::post('/authors/{author}/delete', '\App\Http\Controllers\Web\AuthorController@destroy');
 
+    // Genres
     Route::get('/genres/create', '\App\Http\Controllers\Web\GenreController@create');
     Route::post('/genres/create', '\App\Http\Controllers\Web\GenreController@store');
     Route::get('/genres/{genre}', '\App\Http\Controllers\Web\GenreController@edit');
