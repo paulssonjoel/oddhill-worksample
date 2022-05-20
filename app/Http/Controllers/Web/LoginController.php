1211a5a2f688b5cd961dc42033db8ed56f6b5fd7
Joel Paulsson <105986177+paulssonjoel@users.noreply.github.com>
2022-05-20 20:58:19 +0200

    Add auth

---
+++ app/Http/Controllers/Web/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function form()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        // Reject passwords other than 'pwd'
+        if ($request->input('password') != 'pwd')
+            return back()->withErrors(['password' => 'Invalid credentials']);
+
+        // Set admin status
+        session()->put('isAdmin', true);
+        return redirect('/admin');
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+<x-form-errors />
+
+<form action="" method="POST">
+    <input name="password" type="password" placeholder="password">
+    <input type="submit">
+    @csrf
+</form>
