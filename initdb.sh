3ecfc3421f587f81429df52eead64fc68782f859
Joel Paulsson <105986177+paulssonjoel@users.noreply.github.com>
2022-05-20 15:58:00 +0200

    Also reset DB in initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker exec -it oddhill-mysql bash -c 'mysql -uroot -ppassword database < /source.sql'
+docker exec -it oddhill-mysql bash -c "
+mysql -uroot -ppassword <<< 'DROP DATABASE IF EXISTS \`database\`;CREATE DATABASE \`database\`;'
+mysql -uroot -ppassword database < /source.sql
+"
