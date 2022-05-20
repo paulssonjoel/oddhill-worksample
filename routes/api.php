7b817631520419faeca220ebefbcd3708924589d
Joel Paulsson <105986177+paulssonjoel@users.noreply.github.com>
2022-05-20 21:17:40 +0200

    Refactor author and genre fetching on /api/books

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Book;
 use App\Models\Author;
 use App\Models\Genre;
-use Illuminate\Support\Arr;
 
 Route::get('/books', '\App\Http\Controllers\API\BookController@index');
 Route::get('/books/{book}/openLibrary', '\App\Http\Controllers\API\BookController@viewOpenLibrary');
 Route::get('/books/{book}', fn (Book $book) => $book);
-Route::get('/books/{book}/authors', fn (Book $book) => Arr::pluck($book->load('author_book.author')['author_book'], 'author')); // prettify
-Route::get('/books/{book}/genres', fn (Book $book) => Arr::pluck($book->load('book_genre.genre')['book_genre'], 'genre')); // prettify
+Route::get('/books/{book}/authors', fn (Book $book) => $book->author_book->pluck('author'));
+Route::get('/books/{book}/genres', fn (Book $book) => $book->book_genre->pluck('genre'));
 
 Route::get('/authors', '\App\Http\Controllers\API\AuthorController@index');
 Route::get('/authors/{author}', fn (Author $author) => $author);
