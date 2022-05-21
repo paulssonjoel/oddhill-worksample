c8f37d522eaa9a0e23a29571f1ef8ab95060c45e
Joel Paulsson <105986177+paulssonjoel@users.noreply.github.com>
2022-05-21 19:29:43 +0200

    Add version prefix to API

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,16 @@ use App\Models\Book;
 use App\Models\Author;
 use App\Models\Genre;
 
-Route::get('/books', '\App\Http\Controllers\API\BookController@index');
-Route::get('/books/{book}/openLibrary', '\App\Http\Controllers\API\BookController@viewOpenLibrary');
-Route::get('/books/{book}', fn (Book $book) => $book);
-Route::get('/books/{book}/authors', fn (Book $book) => $book->author_book->pluck('author'));
-Route::get('/books/{book}/genres', fn (Book $book) => $book->book_genre->pluck('genre'));
+Route::prefix('v1')->group(function () {
+    Route::get('/books', '\App\Http\Controllers\API\BookController@index');
+    Route::get('/books/{book}/openLibrary', '\App\Http\Controllers\API\BookController@viewOpenLibrary');
+    Route::get('/books/{book}', fn (Book $book) => $book);
+    Route::get('/books/{book}/authors', fn (Book $book) => $book->author_book->pluck('author'));
+    Route::get('/books/{book}/genres', fn (Book $book) => $book->book_genre->pluck('genre'));
 
-Route::get('/authors', '\App\Http\Controllers\API\AuthorController@index');
-Route::get('/authors/{author}', fn (Author $author) => $author);
+    Route::get('/authors', '\App\Http\Controllers\API\AuthorController@index');
+    Route::get('/authors/{author}', fn (Author $author) => $author);
 
-Route::get('/genres', '\App\Http\Controllers\API\GenreController@index');
-Route::get('/genres/{genre}', fn (Genre $genre) => $genre);
+    Route::get('/genres', '\App\Http\Controllers\API\GenreController@index');
+    Route::get('/genres/{genre}', fn (Genre $genre) => $genre);
+});
